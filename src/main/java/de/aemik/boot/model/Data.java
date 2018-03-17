beb36b37777a15c0d38976965e08f8f8ce6757af
Emanuel Egger <ee@istec.de>
2018-03-17 22:27:31 +0100

    no message

---
+++ src/main/java/de/aemik/boot/model/Data.java
@@ -0,0 +1,12 @@
+package de.aemik.boot.model;
+
+import lombok.AllArgsConstructor;
+
+@lombok.Data
+@AllArgsConstructor
+public class Data {
+	
+	private String id;
+	private String name;
+	private String city;
+}
